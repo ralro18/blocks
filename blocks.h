3f6a5ec7d0f08be1edb6bee18521c55d5602cce5
ralro18 <129241231+ralro18@users.noreply.github.com>
2023-12-03 17:09:58 -0600

    Add files via upload

---
+++ blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb-mem",					30,		0},
+
+	{"",	"sb-vol",					5,		0},
+
+        {"",	"sb-date",					5,		0},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "|";
+static unsigned int delimLen = 5;
